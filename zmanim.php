218f0c8d0532041ec028fc7a281269d69a7adaa0
chabaddev <134392542+chabaddev@users.noreply.github.com>
2023-06-20 00:24:01 +0300

    Update zmanim.php

--- zmanim.php
+++ zmanim.php
@@ -95,7 +95,7 @@
  
   <div id="overlay" style="height:100vh;width:100vw;z-index:10;display:block;position: fixed;top: 0;left: 0;">
     <div style="width:100vw;height:4vh;background-color:rgb(104,10,32);">
-      <h1 style="color:rgb(240,136,39);margin:0px;font-size:60px;">ב"ה</h1>
+      <h1 style="color:rgb(240,136,39);margin:0px;font-size:80px; text-align:right;">ב"ה</h1>
     </div>
     <div style="width:100vw;height:6vh;background-color:rgb(104,10,32);position:absolute;bottom:0px;">
     </div>
