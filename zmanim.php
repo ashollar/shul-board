2d8d13a9842b76407556c192bfee61e599b4b874
chabaddev <134392542+chabaddev@users.noreply.github.com>
2023-06-19 23:10:19 +0300

    Update zmanim.php

--- zmanim.php
+++ zmanim.php
@@ -57,7 +57,7 @@
  
  <div style='background-image:url("background2.jpg");width:100%;height:100%;margin:0px;'>
  
- <div id="txt" style='background-color:white;border-radius:10px;'></div>
+ <div id="frame1" style='background-color:white;border-radius:10px;'></div>
 
  <H2 style="margin:0px;">סוף זמן קריאת שמע</H2>
  <H1 style="margin:0px;"><?php echo $raw['סוף זמן קריאת שמע ']['time'];?></H1>
