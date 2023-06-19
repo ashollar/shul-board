d36f7477f973f58a85443028084da7cc5e9f9647
chabaddev <134392542+chabaddev@users.noreply.github.com>
2023-06-19 16:28:02 +0300

    Update zmanim.php

--- zmanim.php
+++ zmanim.php
@@ -34,7 +34,7 @@
 </script>
  <body onload="initialize()" style="font-size:80px;text-align:center;margin:0px;height:100vh;">
  
- <div style='background-image:url("background2.jpg.jpg");width:100%;height:100%;margin:0px;'>
+ <div style='background-image:url("background2.jpg");width:100%;height:100%;margin:0px;'>
  
  <div id="txt" style='background-color:white;border-radius:10px;'></div>
 
