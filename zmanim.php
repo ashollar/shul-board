d59bdea4f0974d42812381b40783b79b818803ee
chabaddev <134392542+chabaddev@users.noreply.github.com>
2023-06-19 23:25:45 +0300

    Update zmanim.php

--- zmanim.php
+++ zmanim.php
@@ -58,7 +58,7 @@
 <body onload="initialize()" style="font-size:80px;text-align:center;margin:0px;height:100vh;">
  
   <div id="overlay" style="z-index:10;">
-    <H1 id="txt" style='background-color:white;border-radius:10px;'></H1>
+    <H1 id="txt" style=''></H1>
   </div>
 
   <div id="frame1" style='background-image:url("background2.jpg");width:100%;height:100%;margin:0px;z-index:1;'>
