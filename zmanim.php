b94f26d739d3acf3fec29422f9fddc03397f0d59
chabaddev <134392542+chabaddev@users.noreply.github.com>
2023-06-19 23:24:37 +0300

    Update zmanim.php

--- zmanim.php
+++ zmanim.php
@@ -55,29 +55,29 @@
       return i;
     }
 </script>
- <body onload="initialize()" style="font-size:80px;text-align:center;margin:0px;height:100vh;">
+<body onload="initialize()" style="font-size:80px;text-align:center;margin:0px;height:100vh;">
  
- <div style='background-image:url("background2.jpg");width:100%;height:100%;margin:0px;'>
+  <div id="overlay" style="z-index:10;">
+    <H1 id="txt" style='background-color:white;border-radius:10px;'></H1>
+  </div>
+
+  <div id="frame1" style='background-image:url("background2.jpg");width:100%;height:100%;margin:0px;z-index:1;'>
+    <H2 style="margin:0px;">סוף זמן קריאת שמע</H2>
+    <H1 style="margin:0px;"><?php echo $raw['סוף זמן קריאת שמע ']['time'];?></H1>
+
+    <H2 style="margin:0px;">חצות היום</H2>
+    <H1 style="margin:0px;"><?php echo $raw['חצות (היום) ']['time'];?></H1>
+
+    <H2 style="margin:0px;">שקיעת החמה</H2>
+    <H1 style="margin:0px;"><?php echo $raw['שקיעת החמה (שקיעה) ']['time'];?></H1>
+
  
-<div id="txt" style='background-color:white;border-radius:10px;'></div>
+    <H2 style="margin:0px;">צאת הכוכבים</H2>
+    <H1 style="margin:0px;"><?php echo $raw['לילה (צאת הכוכבים) ']['time'];?></H1>
 
-<div id="frame1">
-  <H2 style="margin:0px;">סוף זמן קריאת שמע</H2>
-  <H1 style="margin:0px;"><?php echo $raw['סוף זמן קריאת שמע ']['time'];?></H1>
-
-  <H2 style="margin:0px;">חצות היום</H2>
-  <H1 style="margin:0px;"><?php echo $raw['חצות (היום) ']['time'];?></H1>
-
-  <H2 style="margin:0px;">שקיעת החמה</H2>
-  <H1 style="margin:0px;"><?php echo $raw['שקיעת החמה (שקיעה) ']['time'];?></H1>
-
- 
-  <H2 style="margin:0px;">צאת הכוכבים</H2>
-  <H1 style="margin:0px;"><?php echo $raw['לילה (צאת הכוכבים) ']['time'];?></H1>
-
-  <H2 style="margin:0px;">חצות</H2>
-  <H1 style="margin:0px;"><?php echo $raw['חצות הלילה (הלילה) ']['time'];?></H1>
-</div >
+    <H2 style="margin:0px;">חצות</H2>
+    <H1 style="margin:0px;"><?php echo $raw['חצות הלילה (הלילה) ']['time'];?></H1>
+  </div >
 
 </body>
 </html>
