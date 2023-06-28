d8541206c8131103300ea8c1d276fa78a714fe59
chabaddev <134392542+chabaddev@users.noreply.github.com>
2023-06-28 22:56:52 +0300

    Update utils.php

--- utils.php
+++ utils.php
@@ -299,7 +299,7 @@ function dailystudy(){
     //select useful data and add to new array
     //print_r($rawarray);
     $shiurim=array();
-    print_r($shiurim);
+    print_r($rawarray);
     foreach($rawarray as $item){
         $name=$item['title'];
         $shiurim[$name]=explode(":",$item['description'])[1];;
