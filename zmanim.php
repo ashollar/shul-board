d2672a0a53625b6d9c992ca7da27debf0ff3f196
chabaddev <134392542+chabaddev@users.noreply.github.com>
2023-06-29 11:33:18 +0300

    Update zmanim.php

--- zmanim.php
+++ zmanim.php
@@ -132,7 +132,7 @@
     <H1 style="margin:0px;"><?php echo $zmanim['חצות הלילה (הלילה) ']['time'];?></H1>
     </div>
   </div >
-  <div id="frame2" class="frame" style="font-size:30px;">
+  <div id="frame2" class="frame" style="font-size:60px;">
     <div class="center">
     <H2 style="margin:0px;">חומש</H2>
     <H1 style="margin:0px;"><?php echo $raw['Daily Chumash with Rashi'];?></H1>
